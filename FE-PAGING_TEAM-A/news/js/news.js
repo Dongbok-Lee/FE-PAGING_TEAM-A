d776c0ee6bd4665ba5abe452dd960eda72237694
동복 <typoon0820@gmail.com>
2022-01-28 15:09:44 +0900

    뉴스 메인페이지 완성

--- FE-PAGING_TEAM-A/news/js/news.js
+++ FE-PAGING_TEAM-A/news/js/news.js
@@ -1,1 +1,19 @@
-console.log(news);
+let datas = JSON.parse(JSON.stringify(data));
+console.log(datas)
+
+const newMyArr = datas.articles.forEach((cur, index) => {
+    console.log(index);
+    if(index == 0){
+        var main_card = '<div class = "main_card"><img alt="기사메인이미지"><h2 class = main_news_text></h2></div>'
+        $(".main_article").append(main_card)
+
+        $(".main_card img").attr("src",cur.image)
+        $(".main_news_text").text(cur.title)
+    }else{
+        var article_card =  '<div class = "sub_card"  id = ' + index + '><img alt="기사이미지"><h2 class = sub_news_text></h2></div>'
+        $(".sub_article").append($(article_card))
+
+        $("#" +index+" img").attr("src",cur.image)
+        $("#" +index + " .sub_news_text").text(cur.title)
+    }
+});
