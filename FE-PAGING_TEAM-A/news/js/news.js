c358fb50be313e4e787f459bd009160b7aa49ace
동복 <typoon0820@gmail.com>
2022-01-29 15:23:33 +0900

    기사페이지 완성, 링크연결 완료

--- FE-PAGING_TEAM-A/news/js/news.js
+++ FE-PAGING_TEAM-A/news/js/news.js
@@ -4,15 +4,13 @@ console.log(datas)
 const newMyArr = datas.articles.forEach((cur, index) => {
     console.log(index);
     if(index == 0){
-        var main_card = '<div class = "main_card"><img alt="기사메인이미지"><h2 class = main_news_text></h2></div>'
-        $(".main_article").append(main_card)
-
+        $(".main_article").attr("OnClick","location.href ='news_detail.html?data:0'")
         $(".main_card img").attr("src",cur.image)
-        $(".main_news_text").text(cur.title)
+        $(".main_news_text").append(cur.title)
     }else{
-        var article_card =  '<div class = "sub_card"  id = ' + index + '><img alt="기사이미지"><h2 class = sub_news_text></h2></div>'
+        var article_card =  '<div class = "sub_card"  id = ' + index + ' OnClick = "location.href = \'news_detail.html?data:'+ index +'\'"><img alt="기사이미지"><h2 class = sub_news_text></h2></div>'
         $(".sub_article").append($(article_card))
-
+        
         $("#" +index+" img").attr("src",cur.image)
         $("#" +index + " .sub_news_text").text(cur.title)
     }
---
+++ FE-PAGING_TEAM-A/news/js/news_detail.js
@@ -0,0 +1,20 @@
+let datas = JSON.parse(JSON.stringify(data));
+console.log(datas)
+
+const getParam = () =>{
+    temp = location.href.split("?"); data=temp[1].split(":"); 
+    va = data[1];
+    return va;
+}
+
+const newMyArr = () => {
+    
+    cur = datas.articles[getParam()];
+    $(".news_title").append(cur.title)
+    $(".article_info .date").append(cur.date);
+    $(".article_info .reporter").append(cur.press + " " + cur.reporter);
+    $(".article_image").attr("src",cur.image)
+    $(".article_text").append(cur.contents)
+}
+
+newMyArr();
